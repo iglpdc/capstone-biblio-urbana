4015965366e58d554cd5c078515ef49aba21eed3
Ivan Gonzalez <iglpdc@gmail.com>
2015-01-30 13:28:12 -0600

    Version at the end of the class

--- count-authors.py
+++ count-authors.py
@@ -1,10 +1,19 @@
 # Counts line in a file
 import sys
+import csv
+
 filename = sys.argv[1]
 
-count = 0
-with open(filename, 'r') as reader:
-    for line in reader:
-        count += 1
+def append_last_name_authors(author_list, last_names):
+    '''Get last name from author list'''
+    for author in author_list:
+        last_name = author.split(',')[0].strip()
+        last_names.append(last_name)
 
-print count
+last_names = []
+with open(filename, 'r') as raw:
+    reader = csv.reader(raw)
+    for line in reader:
+        authors_list = line[3].split(';')
+        append_last_name_authors(authors_list, last_names)
+print last_names
