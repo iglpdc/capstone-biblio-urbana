d4595433857483a00b98acecb8955d749a29100d
Ivan Gonzalez <iglpdc@gmail.com>
2015-01-30 14:16:14 -0600

    Improve test script by adding the correct result.

--- test-count-authors.sh
+++ test-count-authors.sh
@@ -1,2 +1,5 @@
 # Testing my script against a few test cases
+echo "The correct count for the test case is:"
+echo "McClelland: 2, Ratcliff: 1, McNaughton: 1, O'Reilly: 1"
+echo "The output of the test case is:"
 python count-authors.py test_biblio.csv
