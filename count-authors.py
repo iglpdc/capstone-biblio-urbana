3dfe3635cdf0b92426ed5a415c2c982b2ecd8f45
Ivan Gonzalez <iglpdc@gmail.com>
2015-01-30 15:22:01 -0600

    Add correct author count.

--- count-authors.py
+++ count-authors.py
@@ -7,8 +7,6 @@ Parameters:
 import sys
 import csv
 
-filename = sys.argv[1]
-
 def append_last_name_authors(author_list, last_names):
     '''Append author's last name to a list'''
     for author in author_list:
@@ -25,7 +23,18 @@ def count_items(items):
             result[item] = 1
     return result
 
+def find_keys_for_value(dictionary, desired_value):
+    '''Finds the key corresponding to a given value in a dict'''
+    result = []
+    for key, value in dictionary.iteritems():
+        if value == desired_value:
+            result.append(key)
+    return result
+
+filename = sys.argv[1]
+
 last_names = []
+
 with open(filename, 'r') as raw:
     reader = csv.reader(raw)
     for line in reader:
@@ -34,5 +43,6 @@ with open(filename, 'r') as raw:
 
 author_counts = count_items(last_names)
 max_times_authored = max(author_counts.values())
-print max_times_authored
-print author_counts
+most_prolific_author = find_keys_for_value(author_counts, max_times_authored)
+
+print "The author with more papers is {0} with {1}.".format(most_prolific_author[0], max_times_authored)
